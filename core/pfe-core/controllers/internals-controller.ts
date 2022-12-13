480c57410067e2002470bffca98e5d81d412bab8
Benny Powers <web@bennypowers.com>
2022-12-13 14:26:00 +0200

    feat(core): proxy elementinternals in internalscontroller

--- core/pfe-core/controllers/internals-controller.ts
+++ core/pfe-core/controllers/internals-controller.ts
@@ -1,12 +1,40 @@
 import type { ReactiveController, ReactiveControllerHost } from 'lit';
 
-export class InternalsController implements ReactiveController {
+export class InternalsController implements ReactiveController, ARIAMixin {
   #internals: ElementInternals;
+
+  declare role: string;
+
+  declare ariaAtomic: ARIAMixin['ariaAtomic'];
+  declare ariaAutoComplete: ARIAMixin['ariaAutoComplete'];
+  declare ariaBusy: ARIAMixin['ariaBusy'];
+  declare ariaChecked: ARIAMixin['ariaChecked'];
+  declare ariaColSpan: ARIAMixin['ariaColSpan'];
+  declare ariaColCount: ARIAMixin['ariaColCount'];
+  declare ariaColIndex: ARIAMixin['ariaColIndex'];
 
   constructor(
     public host: ReactiveControllerHost & HTMLElement,
   ) {
     this.#internals = host.attachInternals();
+    new Proxy(this, {
+      get: (target: InternalsController, key) => {
+        if (key in target.#internals) {
+          return target.#internals[key as keyof ElementInternals];
+        } else {
+          return target[key as keyof InternalsController];
+        }
+      },
+      set(target, key, value) {
+        if (key in target.#internals) {
+          // @ts-expect-error: the effort to type this up properly is not worth it ATM
+          return target.#internals[key] = value;
+        } else {
+          // @ts-expect-error: the effort to type this up properly is not worth it ATM
+          return target[key] = value;
+        }
+      }
+    });
   }
 
   hostConnected?(): void
